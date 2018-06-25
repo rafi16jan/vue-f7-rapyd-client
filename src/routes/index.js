62628324f3ccb9080fda89ec956a5fad8a76b47c
Achmad Kurnianto <achmad.kurnianto@gmail.com>
2018-06-26 01:45:54 +0700

    use async routing

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,6 +2,20 @@ const routes = [{
   path: '/'
 }, {
   path: '/panel/'
+}, {
+  path: '/contacts/',
+  async async (routeTo, routeFrom, resolve, reject) {
+    // dynamic import component; returns promise
+    const component = await import('@/components/Contacts.vue')
+    resolve({ component: component.default })
+  }
+}, {
+  path: '/settings/',
+  async async (routeTo, routeFrom, resolve, reject) {
+    // dynamic import component; returns promise
+    const component = await import('@/components/Users.vue')
+    resolve({ component: component.default })
+  }
 }]
 
 export default routes
