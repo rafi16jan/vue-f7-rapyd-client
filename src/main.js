7c45689ea089863f48002b2f84e72973ab829f14
Achmad Kurnianto <achmad.kurnianto@gmail.com>
2018-07-01 16:15:15 +0700

    add vuex store

--- src/main.js
+++ src/main.js
@@ -5,12 +5,13 @@ import Vue from 'vue'
 import Framework7 from 'framework7/dist/framework7.esm.bundle'
 import Framework7Vue from 'framework7-vue/dist/framework7-vue.esm.bundle'
 import VueSocketIo from 'vue-socket.io'
-
+import './plugins/vuetify'
 import App from './App'
 
+import initStore from './vuex/plugin'
 import routes from './routes'
 
-// import './assets/styles/app.less'
+import './assets/styles/app.less'
 import 'framework7/dist/css/framework7.css'
 import 'ionicons/dist/css/ionicons.css'
 
@@ -22,6 +23,7 @@ Vue.use(VueSocketIo, 'http://localhost:8069')
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
+  store: initStore(),
   components: { App },
   framework7: {
     routes,
@@ -34,6 +36,7 @@ new Vue({
   template: '<App/>',
   sockets: {
     connect () {
+      console.log(this.$store)
       console.log('connected')
       this.$options.sockets.message = (data) => {
         console.log(data)
